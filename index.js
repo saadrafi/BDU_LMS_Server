f579eb0aabf4ac6043c34337fae215d6c6d4fd37
saadrafi <saadrafi81@gmail.com>
2024-01-26 19:51:18 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Hello Server!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
