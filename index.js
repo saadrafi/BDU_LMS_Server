aed2979b2dca3eb98a85f7cfce76b58f2a2ee295
saadrafi <saadrafi81@gmail.com>
2024-02-13 08:55:16 +0600

    create server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-
+const cors = require("cors");
+const port = 5000;
 const morgan = require("morgan");
+require("dotenv").config();
 
 app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello Server!"));
+
+
+app.get("/", (req, res) => res.json("Hello Server!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
