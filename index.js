fd499622562ac14e115a5033a05ebd76cd6c3e7f
saadrafi <saadrafi81@gmail.com>
2024-02-13 09:07:25 +0600

    connect database

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ require("dotenv").config();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+const authRouter = require("./routes/authRouter");
+const db = require("./db");
+app.use("/auth", authRouter);
 
-
+try {
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to database: " + err);
+    } else {
+      console.log("Connected to database!");
+    }
+  });
+} catch (err) {
+  console.log("Error connecting to database: " + err);
+}
 
 app.get("/", (req, res) => res.json("Hello Server!"));
 
